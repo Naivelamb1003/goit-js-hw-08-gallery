5e99294f1e88786fcb47f1a411504b58eed5fd29
Malik Nadiia <59849318+Naivelamb1003@users.noreply.github.com>
2020-10-13 22:28:26 +0300

    Дополнительное задание( прокрутка галерее в модальном окне)

---
+++ js/task-gallery.js
@@ -0,0 +1,60 @@
+import galleryCard from "./gallery-items.js";
+
+const imageHTML = createImageCardsMarkup(galleryCard);
+const galleryContainer = document.querySelector('.js-gallery');
+const openModal = document.querySelector('.lightbox');
+const closeModal = document.querySelector('.lightbox__button');
+const lighboxImage = document.querySelector('.lightbox__image');
+const lighboxOverlay = document.querySelector('.lightbox__overlay');
+
+galleryContainer.insertAdjacentHTML('beforeend', imageHTML);
+
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+closeModal.addEventListener('click', onCloseButtonClick);
+lighboxOverlay.addEventListener('click', onCloseButtonClick);
+
+
+function createImageCardsMarkup(cards) {
+    return cards
+    .map(({ preview, original, description }) => {
+        return `
+     <li class="gallery__item">
+   <a class="gallery__link"
+     href="${original}" >
+     <img
+       class="gallery__image"
+       src="${preview}"
+       data-source="${original}"
+       alt="${description}"
+     />
+   </a>
+ </li>
+    `;
+    })
+        .join('');
+};
+
+function onGalleryContainerClick(evt) {
+    window.addEventListener('keydown', onKeypress);
+    evt.preventDefault();
+    console.log(evt.target);
+
+    lighboxImage.src = evt.target.getAttribute('data-source');
+    openModal.classList.add('is-open');
+        
+};
+
+function onCloseButtonClick() {
+    openModal.classList.remove('is-open');
+    lighboxImage.src = "";
+    window.removeEventListener('keydown', onKeypress);
+};
+
+function onKeypress(evt) {
+    evt.preventDefault();
+    if (evt.code === 'Escape') {
+        onCloseButtonClick();
+    }
+    
+};
+
